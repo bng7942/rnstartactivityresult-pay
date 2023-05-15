540b547d92e3eac80aac4431ef997a10ab5f3a94
bng49890 <bng4989@q-commercelab.com>
2023-05-15 13:04:14 +0900

    startActivityForResult updated

--- android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
+++ android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
@@ -60,15 +60,47 @@ public class RNStartActivityForResultModule extends ReactContextBaseJavaModule {
         mPromise = promise;
 
         try {
-            String intentAction = options.getString("action") == null ? Intent.ACTION_VIEW : options.getString("action");
-            Intent intent = new Intent(intentAction);
+            // String intentAction = options.getString("action") == null ? Intent.ACTION_VIEW : options.getString("action");
+            // Intent launchIntent = new Intent(intentAction);
 
-            if (options.getString("uri") != null) {
-                intent.setData(Uri.parse(options.getString("uri")));
-            }
-            if (options.getMap("extra") != null) {
-                intent.putExtras(Arguments.toBundle(options.getMap("extra")));
-            }
+            // if (options.getString("uri") != null) {
+            //     intent.setData(Uri.parse(options.getString("uri")));
+            // }
+
+            // if (options.getMap("extra") != null) {
+            //     intent.putExtras(Arguments.toBundle(options.getMap("extra")));
+            // }
+
+            String uri = "vpos_app://vpos";
+            Intent launchIntent = new Intent(Intent.ACTION_MAIN, Uri.parse(uri));
+
+            launchIntent.setAction(Intent.ACTION_VIEW);
+            launchIntent.putExtra("byActive", "OutSideAppr");
+            launchIntent.putExtra("byTran", "S0");
+
+            // if (sText.equals("IC신용결제")) launchIntent.putExtra("byTran", "S0");                  // 승인/취소 구분자
+            // else if (sText.equals("IC신용취소"))launchIntent.putExtra("byTran", "S1");             // 승인/취소 구분자
+            // else if ( sText.equals("현금영수증승인")) launchIntent.putExtra("byTran","41");        // 현금영수증 승인 구분자
+            // else if ( sText.equals("현금영수증취소")) launchIntent.putExtra("byTran","42");        // 현금영수증 취소 구분자
+            // else launchIntent.putExtra("byTran", "  ");             // 승인/취소 구분자
+
+            launchIntent.putExtra("byTID", "1046889414");              // 단말기번호
+            launchIntent.putExtra("byInstall", String.format("%02d", Integer.parseInt(inputInstall.getText().toString())));          // 할부개월수
+            launchIntent.putExtra("byAmt", inputAmt.getText().toString());              // 총금액
+            launchIntent.putExtra("byOrgDate", inputOrgDate.getText().toString());          // 원거래일자
+            launchIntent.putExtra("byOrgAuth", inputAuthNo.getText().toString());          // 원거래승인번호
+            launchIntent.putExtra("byTranSerial", getTime().substring(8, 14));          // 거래일련번호
+
+            launchIntent.putExtra("byIdno", inputIdno.getText().toString());          //현금/수표 식별번호
+
+            launchIntent.putExtra("byTaxAmt", "0");          //세금
+            launchIntent.putExtra("bySfeeAmt", "0");          //봉사료
+            launchIntent.putExtra("byFreeAmt", "00000000");      //비과세
+
+            launchIntent.putExtra("byAppCardNum", "                     ");          // APP 카드번호
+            launchIntent.putExtra("bySeumGbun", "  ");          // 세움 거래 구분 / 서명 재사용
+            launchIntent.putExtra("byBUSI", "          ");          //다중사업자번호/사업자번호
+
 
             returnKey = key;
 

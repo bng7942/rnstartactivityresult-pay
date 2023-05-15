5127c2da5d0dcd3a71e59cc4c910cfa711371304
bng49890 <bng4989@q-commercelab.com>
2023-05-15 18:47:28 +0900

    dist deleted

--- dist/constants.d.ts
+++
@@ -1,3 +0,0 @@
-import { IntentParamDefaults, ResponseMessages } from "./interface";
-export declare const INTENT_PARAM_DEFAULT: IntentParamDefaults;
-export declare const RESPONSE_MESSAGE: ResponseMessages;
--- dist/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-import { IntentError, IntentResponse } from "./interface";
-export declare const getError: (message: string, error?: any) => IntentError;
-export declare const getResponse: (response: any) => IntentResponse;

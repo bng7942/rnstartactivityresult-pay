e0e9099636f283f9fd747843db53a3b290c861a3
bng49890 <bng4989@q-commercelab.com>
2023-05-16 09:33:55 +0900

    extra updated

--- android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
+++ android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
@@ -71,52 +71,35 @@ public class RNStartActivityForResultModule extends ReactContextBaseJavaModule {
         mPromise = promise;
 
         try {
-            Bundle bundle = Arguments.toBundle(options);
+            
+            String intentAction = options.getString("action") == null ? Intent.ACTION_VIEW : options.getString("action");
+            String uri = options.getString("uri");
+            Intent intent = new Intent(Intent.ACTION_MAIN, Uri.parse(uri));
+            intent.setAction(Intent.ACTION_VIEW);
+
+            // intent.putExtra("byActive", "OutSideAppr");
+            // intent.putExtra("byTran", "S0");
+            // intent.putExtra("byTID", "1046889414");              // 단말기번호
+            // intent.putExtra("byInstall", "00");          // 할부개월수
+            // intent.putExtra("byAmt", "10");              // 총금액
+            // intent.putExtra("byOrgDate", "");          // 원거래일자
+            // intent.putExtra("byOrgAuth", "");          // 원거래승인번호
+            // intent.putExtra("byTranSerial", getTime().substring(8, 14));          // 거래일련번호
+            // intent.putExtra("byIdno", "");          //현금/수표 식별번호
+            // intent.putExtra("byTaxAmt", "0");          //세금
+            // intent.putExtra("bySfeeAmt", "0");          //봉사료
+            // intent.putExtra("byFreeAmt", "00000000");      //비과세
+            // intent.putExtra("byAppCardNum", "                     ");          // APP 카드번호
+            // intent.putExtra("bySeumGbun", "  ");          // 세움 거래 구분 / 서명 재사용
+            // intent.putExtra("byBUSI", "          ");          //다중사업자번호/사업자번호
+            Bundle bundle = Arguments.toBundle(options.getString("extra"));
             Set<String> keyList = bundle.keySet();
             for (String keys : keyList) {
                 String value = bundle.get(keys).toString();
-                Log.d("VPOS-D","ddddddddddddddddddd" + keys + " | " + value);
+                intent.putExtra(keys, value);
             }
 
-            String intentAction = options.getString("action") == null ? Intent.ACTION_VIEW : options.getString("action");
-            String uri = "vpos_app://vpos";
-            Intent intent = new Intent(Intent.ACTION_MAIN, Uri.parse(uri));
-            intent.setAction(Intent.ACTION_VIEW);
-            intent.putExtra("byActive", "OutSideAppr");
-            intent.putExtra("byTran", "S0");
-
-            // if (sText.equals("IC신용결제")) intent.putExtra("byTran", "S0");                  // 승인/취소 구분자
-            // else if (sText.equals("IC신용취소"))intent.putExtra("byTran", "S1");             // 승인/취소 구분자
-            // else if ( sText.equals("현금영수증승인")) intent.putExtra("byTran","41");        // 현금영수증 승인 구분자
-            // else if ( sText.equals("현금영수증취소")) intent.putExtra("byTran","42");        // 현금영수증 취소 구분자
-            // else intent.putExtra("byTran", "  ");             // 승인/취소 구분자
-
-            intent.putExtra("byTID", "1046889414");              // 단말기번호
-            intent.putExtra("byInstall", "00");          // 할부개월수
-            intent.putExtra("byAmt", "10");              // 총금액
-            intent.putExtra("byOrgDate", "");          // 원거래일자
-            intent.putExtra("byOrgAuth", "");          // 원거래승인번호
-            intent.putExtra("byTranSerial", getTime().substring(8, 14));          // 거래일련번호
-
-            intent.putExtra("byIdno", "");          //현금/수표 식별번호
-
-            intent.putExtra("byTaxAmt", "0");          //세금
-            intent.putExtra("bySfeeAmt", "0");          //봉사료
-            intent.putExtra("byFreeAmt", "00000000");      //비과세
-
-            intent.putExtra("byAppCardNum", "                     ");          // APP 카드번호
-            intent.putExtra("bySeumGbun", "  ");          // 세움 거래 구분 / 서명 재사용
-            intent.putExtra("byBUSI", "          ");          //다중사업자번호/사업자번호
-
             intent.putExtra("byDate", getTime().substring(0, 14));             // 거래 요청 일자
-
-
-            // if (options.getString("uri") != null) {
-            //     intent.setData(Uri.parse(options.getString("uri")));
-            // }
-            // if (options.getMap("extra") != null) {
-            //     intent.putExtras(Arguments.toBundle(options.getMap("extra")));
-            // }
 
             returnKey = key;
 

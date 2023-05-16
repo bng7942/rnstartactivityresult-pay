b20c9b0c34dc6071729951381bd8dcbe1b35c376
bng49890 <bng4989@q-commercelab.com>
2023-05-16 19:32:54 +0900

    bug fix

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 
 declare type startActivityForResult = (key:String, uri:String, action:String, 
-    byTran:String, byTID:String) => void
+    byTran:String, byTID:String, byInstall:String, byAmt:String, byTaxAmt:String, 
+    bySfeeAmt:String, byFreeAmt:String) => void
 
 export = startActivityForResult

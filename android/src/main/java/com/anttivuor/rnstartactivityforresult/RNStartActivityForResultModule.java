e57213823e6588a47ac5cedcf8455feb4cc6c2e2
bng49890 <bng4989@q-commercelab.com>
2023-05-16 18:59:33 +0900

    bug fix

--- android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
+++ android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
@@ -24,7 +24,12 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
 
+import java.util.Iterator;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import android.util.Log;
@@ -126,42 +131,87 @@ public class RNStartActivityForResultModule extends ReactContextBaseJavaModule {
         return json;
     }
   
+    private static WritableArray convertJsonToArray(JSONArray jsonArray) throws JSONException {
+        WritableArray array = new WritableNativeArray();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+        Object value = jsonArray.get(i);
+        if (value instanceof JSONObject) {
+            array.pushMap(convertJsonToMap((JSONObject) value));
+        } else if (value instanceof JSONArray) {
+            array.pushArray(convertJsonToArray((JSONArray) value));
+        } else if (value instanceof Boolean) {
+            array.pushBoolean((Boolean) value);
+        } else if (value instanceof Integer) {
+            array.pushInt((Integer) value);
+        } else if (value instanceof Double) {
+            array.pushDouble((Double) value);
+        } else if (value instanceof String) {
+            array.pushString((String) value);
+        } else {
+            array.pushString(value.toString());
+        }
+        }
+        return array;
+    }
+
+    private static WritableMap convertJsonToMap(JSONObject jsonObject) throws JSONException {
+        WritableMap map = new WritableNativeMap();
+
+        Iterator<String> iterator = jsonObject.keys();
+        while (iterator.hasNext()) {
+        String key = iterator.next();
+        Object value = jsonObject.get(key);
+        if (value instanceof JSONObject) {
+            map.putMap(key, convertJsonToMap((JSONObject) value));
+        } else if (value instanceof JSONArray) {
+            map.putArray(key, convertJsonToArray((JSONArray) value));
+        } else if (value instanceof Boolean) {
+            map.putBoolean(key, (Boolean) value);
+        } else if (value instanceof Integer) {
+            map.putInt(key, (Integer) value);
+        } else if (value instanceof Double) {
+            map.putDouble(key, (Double) value);
+        } else if (value instanceof String) {
+            map.putString(key, (String) value);
+        } else {
+            map.putString(key, value.toString());
+        }
+        }
+        return map;
+    }
+
     private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
         @Override
         public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
             if (requestCode == ACTIVITY_REQUEST_CODE) {
-                String rtn_Tran = data.getStringExtra("rtn_Tran");
-                String rtn_LEDCode = data.getStringExtra("rtn_LEDCode");
-                String rtn_CardBinNum = data.getStringExtra("rtn_CardBinNum");
-                String rtn_Install = data.getStringExtra("rtn_Install");
-                String rtn_AmountNum = data.getStringExtra("rtn_AmountNum");
-                String rtn_TaxNum = data.getStringExtra("rtn_TaxNum");
-                String rtn_SevDate = data.getStringExtra("rtn_SevDate");
-                String rtn_SevTime = data.getStringExtra("rtn_SevTime");
-                String rtn_Authno = data.getStringExtra("rtn_Authno");
-                String rtn_CreditMbrCode = data.getStringExtra("rtn_CreditMbrCode");
-                String rtn_IssuerName = data.getStringExtra("rtn_IssuerName");
-                String rtn_PurchaseName = data.getStringExtra("rtn_PurchaseName");
-                String rtn_ServerMsg1 = data.getStringExtra("rtn_ServerMsg1");
-                String rtn_PrintMsg1 = data.getStringExtra("rtn_PrintMsg1");
-                String rtn_IssuerCode = data.getStringExtra("rtn_IssuerCode");
-                String rtn_PurchaseCode = data.getStringExtra("rtn_PurchaseCode");
-                String rtn_BalPoint = data.getStringExtra("rtn_BalPoint");
-                String rtn_GaPoint = data.getStringExtra("rtn_GaPoint");
-                String rtn_NuPoint = data.getStringExtra("rtn_NuPoint");
-                String rtn_CardTypeGubun = data.getStringExtra("rtn_CardTypeGubun");
-                String rtn_VANUnqTranNum = data.getStringExtra("rtn_VANUnqTranNum");
+                // String rtn_Tran = data.getStringExtra("rtn_Tran");
+                // String rtn_LEDCode = data.getStringExtra("rtn_LEDCode");
+                // String rtn_CardBinNum = data.getStringExtra("rtn_CardBinNum");
+                // String rtn_Install = data.getStringExtra("rtn_Install");
+                // String rtn_AmountNum = data.getStringExtra("rtn_AmountNum");
+                // String rtn_TaxNum = data.getStringExtra("rtn_TaxNum");
+                // String rtn_SevDate = data.getStringExtra("rtn_SevDate");
+                // String rtn_SevTime = data.getStringExtra("rtn_SevTime");
+                // String rtn_Authno = data.getStringExtra("rtn_Authno");
+                // String rtn_CreditMbrCode = data.getStringExtra("rtn_CreditMbrCode");
+                // String rtn_IssuerName = data.getStringExtra("rtn_IssuerName");
+                // String rtn_PurchaseName = data.getStringExtra("rtn_PurchaseName");
+                // String rtn_ServerMsg1 = data.getStringExtra("rtn_ServerMsg1");
+                // String rtn_PrintMsg1 = data.getStringExtra("rtn_PrintMsg1");
+                // String rtn_IssuerCode = data.getStringExtra("rtn_IssuerCode");
+                // String rtn_PurchaseCode = data.getStringExtra("rtn_PurchaseCode");
+                // String rtn_BalPoint = data.getStringExtra("rtn_BalPoint");
+                // String rtn_GaPoint = data.getStringExtra("rtn_GaPoint");
+                // String rtn_NuPoint = data.getStringExtra("rtn_NuPoint");
+                // String rtn_CardTypeGubun = data.getStringExtra("rtn_CardTypeGubun");
+                // String rtn_VANUnqTranNum = data.getStringExtra("rtn_VANUnqTranNum");
 
 
-                // Bundle dataBundle = data.getExtras();
-                // JSONObject jsonObj = bundleToJson(dataBundle);
-                mPromise.resolve(
-                    resultCode.toString(), rtn_Tran, rtn_LEDCode, rtn_CardBinNum, rtn_Install,
-                    rtn_AmountNum, rtn_TaxNum, rtn_SevDate, rtn_SevTime, rtn_Authno,
-                    rtn_CreditMbrCode, rtn_IssuerName, rtn_PurchaseName, rtn_ServerMsg1,
-                    rtn_PrintMsg1, rtn_IssuerCode, rtn_PurchaseCode, rtn_BalPoint,
-                    rtn_GaPoint, rtn_NuPoint, rtn_CardTypeGubun, rtn_VANUnqTranNum
-                );
+                Bundle dataBundle = data.getExtras();
+                JSONObject jsonObj = bundleToJson(dataBundle);
+                WritableMap result = convertJsonToMap(jsonObj);
+                mPromise.resolve(resultCode, result);
 
                 mPromise = null;
 

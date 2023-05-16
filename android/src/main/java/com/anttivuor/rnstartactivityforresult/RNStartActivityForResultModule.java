185abbfe3ec3b6eb1d999506ef37ddaf6b65971f
bng49890 <bng4989@q-commercelab.com>
2023-05-16 18:19:48 +0900

    bug fix

--- android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
+++ android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
@@ -70,6 +70,7 @@ public class RNStartActivityForResultModule extends ReactContextBaseJavaModule {
     public void startActivityForResult(String key, String uri, String action, 
         String byTran, String byTID, String byInstall, String byAmt, 
         String byTaxAmt, String bySfeeAmt, String byFreeAmt, Promise promise) {
+
         Activity currentActivity = getCurrentActivity();
 
         if (currentActivity == null) {
@@ -94,9 +95,9 @@ public class RNStartActivityForResultModule extends ReactContextBaseJavaModule {
             intent.putExtra("byOrgAuth", "");          // 원거래승인번호
             intent.putExtra("byTranSerial", getTime().substring(8, 14));          // 거래일련번호
             intent.putExtra("byIdno", "");          //현금/수표 식별번호
-            intent.putExtra("byTaxAmt", byTaxAmt);          //세금
-            intent.putExtra("bySfeeAmt", bySfeeAmt);          //봉사료
-            intent.putExtra("byFreeAmt", byFreeAmt);      //비과세
+            intent.putExtra("byTaxAmt", "0");          //세금
+            intent.putExtra("bySfeeAmt", "0");          //봉사료
+            intent.putExtra("byFreeAmt", "00000000");      //비과세
             intent.putExtra("byAppCardNum", "                     ");          // APP 카드번호
             intent.putExtra("bySeumGbun", "  ");          // 세움 거래 구분 / 서명 재사용
             intent.putExtra("byBUSI", "          ");          //다중사업자번호/사업자번호
@@ -129,9 +130,39 @@ public class RNStartActivityForResultModule extends ReactContextBaseJavaModule {
         @Override
         public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
             if (requestCode == ACTIVITY_REQUEST_CODE) {
-                Bundle dataBundle = data.getExtras();
-                JSONObject jsonObj = bundleToJson(dataBundle);
-                mPromise.resolve(jsonObj);
+                String rtn_Tran = data.getStringExtra("rtn_Tran");
+                String rtn_LEDCode = data.getStringExtra("rtn_LEDCode");
+                String rtn_CardBinNum = data.getStringExtra("rtn_CardBinNum");
+                String rtn_Install = data.getStringExtra("rtn_Install");
+                String rtn_AmountNum = data.getStringExtra("rtn_AmountNum");
+                String rtn_TaxNum = data.getStringExtra("rtn_TaxNum");
+                String rtn_SevDate = data.getStringExtra("rtn_SevDate");
+                String rtn_SevTime = data.getStringExtra("rtn_SevTime");
+                String rtn_Authno = data.getStringExtra("rtn_Authno");
+                String rtn_CreditMbrCode = data.getStringExtra("rtn_CreditMbrCode");
+                String rtn_IssuerName = data.getStringExtra("rtn_IssuerName");
+                String rtn_PurchaseName = data.getStringExtra("rtn_PurchaseName");
+                String rtn_ServerMsg1 = data.getStringExtra("rtn_ServerMsg1");
+                String rtn_PrintMsg1 = data.getStringExtra("rtn_PrintMsg1");
+                String rtn_IssuerCode = data.getStringExtra("rtn_IssuerCode");
+                String rtn_PurchaseCode = data.getStringExtra("rtn_PurchaseCode");
+                String rtn_BalPoint = data.getStringExtra("rtn_BalPoint");
+                String rtn_GaPoint = data.getStringExtra("rtn_GaPoint");
+                String rtn_NuPoint = data.getStringExtra("rtn_NuPoint");
+                String rtn_CardTypeGubun = data.getStringExtra("rtn_CardTypeGubun");
+                String rtn_VANUnqTranNum = data.getStringExtra("rtn_VANUnqTranNum");
+
+
+                // Bundle dataBundle = data.getExtras();
+                // JSONObject jsonObj = bundleToJson(dataBundle);
+                mPromise.resolve(
+                    resultCode, rtn_Tran, rtn_LEDCode, rtn_CardBinNum, rtn_Install,
+                    rtn_AmountNum, rtn_TaxNum, rtn_SevDate, rtn_SevTime, rtn_Authno,
+                    rtn_CreditMbrCode, rtn_IssuerName, rtn_PurchaseName, rtn_ServerMsg1,
+                    rtn_PrintMsg1, rtn_IssuerCode, rtn_PurchaseCode, rtn_BalPoint,
+                    rtn_GaPoint, rtn_NuPoint, rtn_CardTypeGubun, rtn_VANUnqTranNum
+                );
+
                 mPromise = null;
 
                 // if (resultCode == MSG_STATE_OK) {

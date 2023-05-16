b2b743c727dfe285472fbcc2a25dd832b98f29b4
bng49890 <bng4989@q-commercelab.com>
2023-05-16 23:30:15 +0900

    bug fix

--- android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
+++ android/src/main/java/com/anttivuor/rnstartactivityforresult/RNStartActivityForResultModule.java
@@ -75,7 +75,7 @@ public class RNStartActivityForResultModule extends ReactContextBaseJavaModule {
     public void startActivityForResult(String key, String uri, String action, 
         String byTran, String byTID, String byInstall, String byAmt, 
         String byTaxAmt, String bySfeeAmt, String byFreeAmt, String oriDate, 
-        String byOrgAuth, String idno Promise promise) {
+        String byOrgAuth, String idno, Promise promise) {
 
         Activity currentActivity = getCurrentActivity();
 

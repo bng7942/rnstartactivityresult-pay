fa7b68e7b669db28148ca6ecdaa9c72f6675c156
bng49890 <bng4989@q-commercelab.com>
2023-05-17 10:35:18 +0900

    updated

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,18 @@
 
-declare type startActivityForResult = (key:String, uri:String, action:String, 
-    byTran:String, byTID:String, byInstall:String, byAmt:String, byTaxAmt:String, 
-    bySfeeAmt:String, byFreeAmt:String, byOrgDate:String, byOrgAuth:String,
-    byIdno:String) => void
+declare type startActivityForResult = (
+    key:String, 
+    uri:String, 
+    action:String, 
+    byTran:String, 
+    byTID:String, 
+    byInstall:String, 
+    byAmt:String, 
+    byTaxAmt:String, 
+    bySfeeAmt:String, 
+    byFreeAmt:String, 
+    byOrgDate:String, 
+    byOrgAuth:String,
+    byIdno:String
+) => void
 
 export = startActivityForResult
--- index.js
+++ index.js
@@ -5,9 +5,15 @@
 import { NativeModules } from "react-native";
 var RNStartActivityForResult = NativeModules.RNStartActivityForResult || {};
 
-var startActivityForResult = (key, uri, action, byTran, byTID, byInstall, byAmt, byTaxAmt, bySfeeAmt, byFreeAmt, byOrgDate, byOrgAuth, byIdno) => {
+var startActivityForResult = (
+    key, uri, action, byTran, byTID, byInstall, 
+    byAmt, byTaxAmt, bySfeeAmt, byFreeAmt, 
+    byOrgDate, byOrgAuth, byIdno) => {
     
-    return RNStartActivityForResult.startActivityForResult(key, uri, action, byTran, byTID, byInstall, byAmt, byTaxAmt, bySfeeAmt, byFreeAmt, byOrgDate, byOrgAuth, byIdno);
+    return RNStartActivityForResult.startActivityForResult(
+        key, uri, action, byTran, byTID, byInstall, byAmt, byTaxAmt, 
+        bySfeeAmt, byFreeAmt, byOrgDate, byOrgAuth, byIdno
+    );
 };
 
 export default startActivityForResult;
